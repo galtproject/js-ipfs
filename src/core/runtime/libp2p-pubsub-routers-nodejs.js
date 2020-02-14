93ca702a564429d984f6c79632fd8431f436c0f2
jonybang <Jonybange@gmail.com>
2020-02-14 23:00:25 +0200

    improve pubsub

--- src/core/runtime/libp2p-pubsub-routers-nodejs.js
+++ src/core/runtime/libp2p-pubsub-routers-nodejs.js
@@ -1,6 +1,122 @@
 'use strict'
 
-module.exports = {
-  gossipsub: require('libp2p-gossipsub'),
-  floodsub: require('libp2p-floodsub')
+const Gossib = require('libp2p-gossipsub');
+const Floodsub = require('libp2p-floodsub');
+const pMap = require('p-map')
+
+const { utils } = require('libp2p-pubsub');
+const ensureArray = utils.ensureArray;
+// const utils = require('libp2p-pubsub/src/utils')
+const { signMessage } = require('libp2p-pubsub/src/message/sign');
+
+class ImprovedGossib extends Gossib {
+  _buildMessage (message) {
+    return this._buildMessageByPeerId(this.peerId, message);
+  }
+
+  _buildMessageByPeerId (peerId, message) {
+    const msg = utils.normalizeOutRpcMessage(message)
+    if (this.peerId) {
+      return signMessage(peerId, msg)
+    } else {
+      return message
+    }
+  }
+
+  async publishByPeerId (peerId, topics, messages) {
+    if (!this.started) {
+      throw new Error('Pubsub has not started')
+    }
+
+    this.log('publish', topics, messages)
+
+    topics = utils.ensureArray(topics)
+    messages = utils.ensureArray(messages)
+
+    const from = this.peerInfo.id.toB58String()
+
+    const buildMessage = (msg, cb) => {
+      const seqno = utils.randomSeqno()
+      const msgObj = {
+        from: from,
+        data: msg,
+        seqno: seqno,
+        topicIDs: topics
+      }
+      // put in seen cache
+      this.seenCache.put(msgObj.seqno)
+
+      // Emit to self if I'm interested and emitSelf enabled
+      this._options.emitSelf && this._emitMessages(topics, [msgObj])
+
+      return this._buildMessageByPeerId(peerId, msgObj)
+    }
+    const msgObjects = await pMap(messages, buildMessage)
+
+    // send to all the other peers
+    this._publish(utils.normalizeOutRpcMessages(msgObjects))
+  }
+
+  async publish(topics, messages) {
+    return this.publishByPeerId(this.peerId, topics, messages);
+  }
 }
+
+class ImprovedFloodsub extends Floodsub {
+  _buildMessage (message) {
+    return this._buildMessageByPeerId(this.peerId, message);
+  }
+
+  _buildMessageByPeerId (peerId, message) {
+    const msg = utils.normalizeOutRpcMessage(message)
+    if (this.peerId) {
+      return signMessage(peerId, msg)
+    } else {
+      return message
+    }
+  }
+
+  async publishByPeerId (peerId, topics, messages) {
+    if (!this.started) {
+      throw new Error('FloodSub is not started')
+    }
+
+    log('publish', topics, messages)
+
+    topics = ensureArray(topics)
+    messages = ensureArray(messages)
+
+    const from = this.peerInfo.id.toB58String()
+
+    const buildMessage = (msg) => {
+      const seqno = utils.randomSeqno()
+      this.seenCache.put(utils.msgId(from, seqno))
+
+      const message = {
+        from: from,
+        data: msg,
+        seqno: seqno,
+        topicIDs: topics
+      }
+
+      // Emit to self if I'm interested and it is enabled
+      this._options.emitSelf && this._emitMessages(topics, [message])
+
+      return this._buildMessageByPeerId(peerId, message)
+    }
+
+    const msgObjects = await pMap(messages, buildMessage)
+
+    // send to all the other peers
+    this._forwardMessages(topics, msgObjects)
+  }
+
+  async publish(topics, messages) {
+    return this.publishByPeerId(this.peerId, topics, messages);
+  }
+}
+
+module.exports = {
+  gossipsub: ImprovedGossib,
+  floodsub: ImprovedFloodsub
+};
