1b15af9f96447a13ed85b707befe9d8622eb7c0b
jonybang <jonybange@gmail.com>
2020-12-17 22:18:36 +0200

    add publishMessage

--- packages/ipfs-core/src/components/pubsub.js
+++ packages/ipfs-core/src/components/pubsub.js
@@ -13,6 +13,12 @@ module.exports = ({ libp2p }) => {
       }
       await libp2p.pubsub.publish(topic, data)
     }),
+    publishMessage: withTimeoutOption(async (topic, message, options) => {
+      if (!data) {
+        throw errCode(new Error('argument "data" is required'), 'ERR_ARG_REQUIRED')
+      }
+      await libp2p.pubsub.publishMessage(topic, message)
+    }),
     ls: withTimeoutOption((...args) => libp2p.pubsub.getTopics(...args)),
     peers: withTimeoutOption((...args) => libp2p.pubsub.getSubscribers(...args))
   }
